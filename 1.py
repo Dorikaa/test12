74b4cbca59040afb9ddd4c3c2fb9eb96c7fbf5fa
horatiu <horatiu.buricea@gmail.com>
2019-10-22 22:38:56 +0300

    initial commit

--- 1.py
+++ 1.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+import requests
+
 a = []
 
 for i in range(2000, 3201):
-    if i // 7 and (i%5 != 0):
+    if i // 7 and (i % 5 != 0):
         a.append(str(i))
 
-print(','.join(a))
+# print(','.join(a))
+
+r = requests.get("https://google.ro")
+# print(r.status_code)
+
+name = input("Your name? ")
+print("your name is:", name)
